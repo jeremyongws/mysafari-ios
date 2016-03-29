359ff6499d30577930192ff14ab9aceacef4ed14
Jeremy Ong <jeremy.ongws@gmail.com>
2016-03-29 15:08:09 +0800

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Jeremy Ong on 29/03/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Jeremy Ong on 29/03/2016.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
